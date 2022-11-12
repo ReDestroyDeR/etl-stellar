9244e2538176f0f0f8e9843193ecfe133e198c66
ReDestroyDeR <daniil.schroder@gmail.com>
2022-11-12 22:29:01 +0300

    Redefine protocol

--- src/main/scala/actor/ExtractorActor.scala
+++ src/main/scala/actor/ExtractorActor.scala
@@ -1,4 +1,12 @@
 package ru.red.etl_stellar
 package actor
 
-case object ExtractorActor
+object ExtractorActor {
+  sealed trait ExtractorProtocol
+  final case class ExtractLedgers(startSequenceNumber: Int, endSequenceNumber: Int)
+
+  sealed trait ExtractorState
+  final case class Batch() extends ExtractorState
+  final case class RealTime() extends ExtractorState
+  final case class Idle() extends ExtractorState
+}
